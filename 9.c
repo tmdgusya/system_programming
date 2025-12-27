fd443b427ff4787e1f5a4ed4c69f28964428ccda
tmdgusya <dev0jsh@gmail.com>
2025-12-27 13:47:28 +0900

    Truncate

---
+++ 9.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main(void) {
+    int ret;
+    ret = truncate("pirate.txt", 45);
+    if (ret == -1) {
+        perror("truncate");
+        return -1;
+    }
+    return 0;
+}
